3ca0fcd0f8c8ba59d18e88b905545659197832cb
Max Ischenko <ischenko@gmail.com>
2024-12-28 10:25:20 +0200

    added docs from the README

--- journal.py
+++ journal.py
@@ -8,6 +8,12 @@
 # Install uv:
 # https://github.com/astral-sh/uv 
 
+"""
+Read entries from Apple Journal app and combines them into a single .html file. 
+
+To use the script export your Apple Journal entries from the phone first and unzip them-the script is looking for Entries folder with  *.html files. The script will save the results into index.html, it will also strip all extra formatting and img tags.
+"""
+
 
 import os
 from bs4 import BeautifulSoup
